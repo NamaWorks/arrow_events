bf16d7189ff58dfe02d9842369f048e6921600c9
NamaWorks <pedronayamartinez@hotmail.com>
2024-05-10 17:00:14 +0200

    fix populate problem in events

--- src/backend/api/routes/event_routes.js
+++ src/backend/api/routes/event_routes.js
@@ -1,6 +1,7 @@
 const { isAuth } = require("../../middleware/auth");
 const { isAdmin } = require("../../middleware/is-admin");
 const { getEvents, updateEvent, removeEvent, postEvent } = require("../controllers/event_controller");
+const Event = require("../models/event_model");
 const User = require("../models/user_model");
 
 const eventsRouter = require("express").Router()
@@ -9,15 +10,15 @@ eventsRouter.get("/all", getEvents)
 eventsRouter.post("/new", postEvent)
 eventsRouter.put("/update/:id", updateEvent)
 eventsRouter.delete("/remove/:id", removeEvent)
-
 eventsRouter.get("/", async (req, res, next) => {
   try {
-    const makers = await User.find().populate("attendants")
-    console.log(makers);
-    return res.status(200).json(makers)
+    const events = await Event.find().populate('attendants')
+    console.log(events);
+    return res.status(200).json(events)
   } catch (err) {
     return next(err)
   }
 })
+
 
 module.exports = eventsRouter
